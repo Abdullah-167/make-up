22a2261d4d4190f60ad908d4f0773c8a5e445c5d
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-10-02 15:57:36 +0500

    comit

--- src/Components/Views/About/OurMission.tsx
+++ src/Components/Views/About/OurMission.tsx
@@ -12,7 +12,7 @@ const OurMission = () => {
                     <div className='flex flex-wrap md:flex-nowrap justify-between items-center pb-10 '>
                         <div className=''>
                             <h1 className='text-3xl sm:text-6xl font-semibold max-w-[500px] sm:leading-[60px] pb-5'>Our <span className=' text-tertiary'> Mission </span></h1>
-                            <h2 className=' max-w-[450px] pb-5 leading-6'>Our mission is simple: to demystify the makeup world and make it accessible to everyone. We are committed to providing the knowledge and inspiration you need to become your makeup artist. Makeup should enhance your unique features and reflect your style.</h2>
+                            <h2 className=' max-w-[450px] pb-5 leading-6'>{`Our mission is simple: to demystify the makeup world and make it accessible to everyone. We are committed to providing the knowledge and inspiration you need to become your makeup artist. Makeup should enhance your unique features and reflect your style.`}</h2>
                             <Button
                                 btnText={`Let's Explore`}
                                 borderRadius={'10px'}
@@ -25,7 +25,7 @@ const OurMission = () => {
                             />
                         </div>
                         <div className='md:flex hidden'>
-                            <Image loading='lazy' src={'/our-mission.jpg'} className='pointer-events-none' alt='her0-img' width={600} height={600} />
+                            <Image loading='lazy' src={'/our-mission.jpg'} className='pointer-events-none' alt='our-mission-img' width={600} height={600} />
                         </div>
                     </div>
                 </Container>
