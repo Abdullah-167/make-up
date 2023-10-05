119942bcc66c99a5dfd4fb921ad0941446cfeea0
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-10-05 18:51:58 +0500

    commit

--- src/Components/Common/Comment.tsx
+++ src/Components/Common/Comment.tsx
@@ -153,7 +153,7 @@ const Comment = () => {
                 <div className='max-w-[970px] md:pl-[180px]'>
                     <div>
                         <p className='text-3xl  pb-2'>Leave a <span className='text-tertiary'>Comment</span> </p>
-                        <p className='text-sm pb-4 max-w-[450px]'>We value your <span className=' text-tertiary'> opinion! </span> Please share your thoughts and the experiences you've gained after reading this article below:</p>
+                        <p className='text-sm pb-4 max-w-[450px]'>We value your <span className=' text-tertiary'> opinion! </span> {`Please share your thoughts and the experiences you've gained after reading this article below:`}</p>
                         <div className='flex gap-2 items-center'>
                             <span>
                                 Article   Rating
