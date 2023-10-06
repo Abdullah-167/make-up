74bf3ddd91a5dc27b818a43e4d5f2d3c07f3d284
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-10-06 13:22:51 +0500

    commit done

--- src/Components/Views/Contact/index.tsx
+++ src/Components/Views/Contact/index.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
         <Layout>
             <Hero />
             <Form />
-            <LatestBlog />
+            {/* <LatestBlog /> */}
             <Instagram />
         </Layout>
     )
