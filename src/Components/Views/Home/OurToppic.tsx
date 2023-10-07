9d37d22df6b9225b6139b29800af477142b2eecf
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-10-07 10:05:10 +0500

    commit done

--- src/Components/Views/Home/OurToppic.tsx
+++ src/Components/Views/Home/OurToppic.tsx
@@ -45,7 +45,7 @@ const OurTopic: React.FC<OurTopicProps> = () => {
 
         headings?.forEach((heading) => {
             const rect = heading.getBoundingClientRect();
-            if (rect.top >= 100 && rect.bottom <= window.innerHeight - 100) {
+            if (rect.top >= 100 && rect.bottom <= window.innerHeight - 300) {
                 setActiveType(heading.getAttribute('data-type') || '');
             }
         });
